6bb8e03eaaf3378d8c7e0a27ecfd1895da8fb5a6
NagnathSomanwar <nagnathsomanwar@gmail.com>
2024-09-30 16:08:25 +0530

    Atomacticaly created user profile

--- musker/models.py
+++ musker/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 # Create a User Profile Model
@@ -12,5 +14,14 @@ class Profile(models.Model):
         return self.user.username
 
 
+# Create Profile when user Signs Up
+# @receiver(post_save,sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+        # have the user fallow themselves
+        user_profile.fallows.set([instance.profile.user.id])
 
 
+post_save.connect(create_profile, sender=User)
