86d3c794262c7921670a6e9c695b53c82e2661da
NagnathSomanwar <nagnathsomanwar@gmail.com>
2024-10-23 17:03:12 +0530

    Login page

--- musker/urls.py
+++ musker/urls.py
@@ -3,5 +3,7 @@ from . import views
 urlpatterns=[
     path('',views.home, name='home'),
     path('profile_list/',views.profile_list,name='profile_list'),
-    path('profile/<int:pk>',views.profile,name='profile')
+    path('profile/<int:pk>',views.profile,name='profile'),
+    path('user_login',views.user_login,name='login'),
+    path('user_logout',views.user_logout,name='logout'),
 ]
--- musker/views.py
+++ musker/views.py
@@ -1,25 +1,26 @@
 from django.shortcuts import render, redirect
-from .models import Profile,Meep
+from .models import Profile, Meep
 from django.contrib import messages
 from .forms import MeepForm
+from django.contrib.auth import authenticate, login, logout
+
 
 # Create your views here.
-
 def home(request):
-    print(request.user.username)
     if request.user.is_authenticated:
         form = MeepForm(request.POST or None)
         if request.method == 'POST':
             if form.is_valid():
-                meep=form.save(commit=False)
-                meep.user=request.user
+                meep = form.save(commit=False)
+                meep.user = request.user
                 meep.save()
                 return redirect('home')
-        meeps=Meep.objects.all().order_by('-id')
-        return render(request, 'home.html', {'meeps': meeps,'form':form})
+        meeps = Meep.objects.all().order_by('-id')
+        return render(request, 'home.html', {'meeps': meeps, 'form': form})
     else:
-        meeps=Meep.objects.all()
+        meeps = Meep.objects.all()
         return render(request, 'home.html', {'meeps': meeps})
+
 
 def profile_list(request):
     if request.user.is_authenticated:
@@ -32,24 +33,44 @@ def profile_list(request):
 
 def profile(request, pk):
     if request.user.is_authenticated:
-        profile = Profile.objects.get(user_id=pk) # Soni
+        profile = Profile.objects.get(user_id=pk)  # Soni
 
-        meeps=Meep.objects.filter(user_id=pk)
+        meeps = Meep.objects.filter(user_id=pk)
 
-        #Post Form Logic
-        if request.method =="POST":
+        # Post Form Logic
+        if request.method == "POST":
             # Get current.ID
-            current_user_profile =request.user.profile
+            current_user_profile = request.user.profile
             # Get form data
-            action=request.POST['follow']
-            #Decide to follow or unfollow
-            if action =='unfollow':
+            action = request.POST['follow']
+            # Decide to follow or unfollow
+            if action == 'unfollow':
                 current_user_profile.fallows.remove(profile)
             elif action == 'follow':
                 current_user_profile.fallows.add(profile)
             # Save the Profile
             current_user_profile.save()
-        return render(request, 'profile.html', {'profile': profile,'meeps':meeps})
+        return render(request, 'profile.html', {'profile': profile, 'meeps': meeps})
     else:
         messages.success(request, ('you Must Be Logged in To view This '))
         return redirect('home')
+
+
+def user_login(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            success = messages.success(request, 'You have Been Logged In Successfully')
+            return redirect('home')
+        else:
+            error = messages.error(request, 'Some Thing went Wrong Please Try Again after Sometime')
+    return render(request, 'login.html', {})
+
+
+def user_logout(request):
+    logout(request)
+    success = messages.success(request, 'You are Logged Out... Now')
+    return redirect('home')
